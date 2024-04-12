25cc5332bc89ada4b79b7f091bfd6ae1fefa1f55
CDboyOne <CDboyOne@outlook.com>
2024-04-12 22:27:55 +0800

    init

---
+++ MultiThread/Program.cs
@@ -0,0 +1,20 @@
+﻿using NBomber.CSharp;
+using NBomber.Contracts;
+
+Console.WriteLine("Hello, World!");
+
+var scenario = Scenario.Create("MultiThread", scenectx =>
+{
+    int threadIndex = scenectx.ScenarioInfo.InstanceNumber;
+    int callIndex = (int)scenectx.InvocationNumber - 1;
+
+    scenectx.Logger.Warning($"THREAD INDEX {threadIndex} CALL INDEX {callIndex}");
+    
+    return Task.FromResult(Response.Ok() as IResponse);
+})
+.WithLoadSimulations(
+    Simulation.IterationsForConstant(copies: 4, iterations: 4)
+)
+.WithoutWarmUp();
+
+NBomberRunner.RegisterScenarios(scenario).Run();
